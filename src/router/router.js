ea1f5dfbf9ada3b3a2153f8137f4d40bda5aac49
megait004 <giapfc123@gmail.com>
2026-03-03 20:44:44 +0700

    ..

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,9 @@
 import { createBrowserRouter } from 'react-router';
 import Index from '@/pages/index';
-import Home from '@/pages/home';
 import NotFound from '@/pages/not-found';
 
 export const PATHS = {
     INDEX: '/',
-    HOME: '/home',
     TIMEACTIVE: '/live'
 };
 
@@ -14,10 +12,7 @@ const router = createBrowserRouter([
         path: PATHS.INDEX,
         element: <NotFound />
     },
-    {
-        path: PATHS.HOME,
-        element: <Home />
-    },
+
     {
         path: `${PATHS.TIMEACTIVE}/*`,
         element: <Index />
